8740872f90f4870a2cbe78ac31bbc5409a7df757
buidinhkha <57929886+buidinhkha@users.noreply.github.com>
2019-11-21 12:56:04 +0800

    Update prolife.php

--- prolife.php
+++ prolife.php
@@ -6,14 +6,22 @@
 <body>
 
 <h1 style="color: blue"; size=20 >Enter Your info </h1>
-Name: <input type="text"  id="name">
+
+Name:   <input type="text"  id="name">
 <br>
 School: <input type="text"  id="school">
 <br>
 <input type="button" value="Ok" onclick="display()">
 
 <script type="text/javascript">
+	function checkInput(){
+         var name = document.getElementById('name').value;
+         if (name.length < 5){
+         	alert('name is less than 5 characters');
+         }
+		}
 	function display(){
+		checkInput();
 		var name = document.getElementById('name').value;
 		var school = document.getElementById('school').value;
 		alert('welcome ' + name + ' from school ' + school);
