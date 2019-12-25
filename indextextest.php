260024d53f61da449733e6101c5c98ee5f046274
buidinhkha <57929886+buidinhkha@users.noreply.github.com>
2019-12-25 18:09:43 +0800

    Update indextextest.php

--- indextextest.php
+++ indextextest.php
@@ -1,6 +1,6 @@
 <?php 
 require_once("funtion.php");
-$db = parse_url("postgres://rxfjbfsqrsviad:9b3a58af90e2f26e82fc091368331f6fc804bf01febdb080b1b21f392c88544a@ec2-54-225-205-79.compute-1.amazonaws.com:5432/dd973qr28s5tg0");
+$db = parse_url(getenv("DATABASE_URL"));
 	
 		
 			$sqlx = "SELECT * from product where productid=10";
