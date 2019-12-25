b2c62c4d373e78ff1a98a1b3689de8c0de34206c
buidinhkha <57929886+buidinhkha@users.noreply.github.com>
2019-12-25 18:10:25 +0800

    Update funtion.php

--- funtion.php
+++ funtion.php
@@ -1,5 +1,5 @@
 <?php 
-	$DATABASE="postgres://rxfjbfsqrsviad:9b3a58af90e2f26e82fc091368331f6fc804bf01febdb080b1b21f392c88544a@ec2-54-225-205-79.compute-1.amazonaws.com:5432/dd973qr28s5tg0"
+	$DATABASE="postgres://rxfjbfsqrsviad:9b3a58af90e2f26e82fc091368331f6fc804bf01febdb080b1b21f392c88544a@ec2-54-225-205-79.compute-1.amazonaws.com:5432/dd973qr28s5tg0";
 	function query($sql)
 	{
 		$db = parse_url($DATABASE_URL);//get environment variable
